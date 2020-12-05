630b47e4f9f3d4049ccbb381e8d10401804ac1e9
加藤恵 <>
2020-12-05 22:03:51 +0800

    enter

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,11 @@
 <template>
-    <div>
-        <div class="nav">
-            <img src="../assets/img/BlessingSoftware_Logo.svg" alt="">
-            <router-link to="/">HOME</router-link>
+    <div class="nav">
+        <div id="nav">
+            <router-link to="/">
+                <img src="../assets/img/blessingsoftware-logo.svg" alt="">
+            </router-link>
             <router-link to="/news">NEWS</router-link>
             <router-link to="/store">STORE</router-link>
-        </div>
-        <div class="menu">
-            <div id="mebu_bar">
-                <div id="menu">
-                    <span class="menu_bar"></span>
-                    <span class="menu_bar"></span>
-                    <span class="menu_bar"></span>
-                    <span class="menu_text">MENU</span>
-                </div>
-            </div>
         </div>
     </div>
 </template>
