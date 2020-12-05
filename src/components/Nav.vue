55444810de36f763ad06d014a33f149aa4d90956
加藤恵 <>
2020-12-05 22:16:56 +0800

    enter

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="nav">
         <div id="nav">
-            <router-link to="/">
+            <router-link to="/" id="nav_link">
                 <img src="../assets/img/blessingsoftware-logo.svg" alt="">
             </router-link>
-            <router-link to="/news">NEWS</router-link>
-            <router-link to="/store">STORE</router-link>
+            <router-link to="/news" id="nav_link">NEWS</router-link>
+            <router-link to="/store" id="nav_link">STORE</router-link>
         </div>
     </div>
 </template>
