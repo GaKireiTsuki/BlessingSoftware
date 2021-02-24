015dcaa7f78b58644bb35d8341cddefc64eaada0
加藤恵 <>
2021-02-24 16:19:06 +0800

    enter

--- src/request/api/base.js
+++ src/request/api/base.js
@@ -1,6 +1,6 @@
 const base = {
-    music: 'https://music_api.hxmusic.xyz'
-    /* music: 'http://localhost:3000' */
+    /* music: 'https://music_api.hxmusic.xyz' */
+    music: 'http://localhost:3000'
 }
 
 export default base
